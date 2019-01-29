b21d2d6c0bade0f60a77b0ec0070d8e8aa2abe7e
Yassine KLILICH <klilichyassine8@gmail.com>
2019-01-29 23:49:20 +0100

    Completing from the standard design

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import $ from 'jquery';
+import '@fortawesome/fontawesome-free/css/all.css'
 import '../style.css';
 
 class App extends Component {
@@ -18,22 +19,28 @@ class App extends Component {
 			return (
 				<div className="container">
 					<div className="clearfix">
-						<button className="btn btn-primary float-right rounded-circle" data-toggle="tooltip">+</button>
+						<button className="btn btn-primary float-right rounded-circle" data-toggle="tooltip"><i className="fas fa-plus"></i></button>
 					</div>
 					<div className="todo-container">
 						<div className="row border border-bottom-0 border-primary rounded">
-							<span className="p-2"><input type="checkbox"/></span>
+							<div className="custom-control custom-checkbox py-2 pr-2">
+								<input type="checkbox" className="custom-control-input" id="customCheck"/>
+								<label className="custom-control-label" htmlFor="customCheck"></label>
+							</div>
 							<span className="col text-truncate py-2 pl-2">Do my homeworks</span>
-							<span className="p-2">STAR</span>
-							<span className="p-2">EDIT</span>
-							<span className="p-2">DELETE</span>
+							<span className="p-2"><i className="far fa-star fa-lg"></i></span>
+							<span className="p-2"><i className="far fa-edit fa-lg"></i></span>
+							<span className="p-2 pr-3"><i className="far fa-trash-alt fa-lg"></i></span>
 						</div>
 						<div className="row border border-bottom-0 border-primary rounded">
-							<span className="p-2"><input type="checkbox"/></span>
+							<div className="custom-control custom-checkbox py-2 pr-2">
+								<input type="checkbox" className="custom-control-input" id="customCheck2"/>
+								<label className="custom-control-label" htmlFor="customCheck2"></label>
+							</div>
 							<span className="col text-truncate py-2 pl-2">Do shopping with my mother</span>
-							<span className="p-2">STAR</span>
-							<span className="p-2">EDIT</span>
-							<span className="p-2">DELETE</span>
+							<span className="p-2"><i className="far fa-star fa-lg"></i></span>
+							<span className="p-2"><i className="far fa-edit fa-lg"></i></span>
+							<span className="p-2 pr-3"><i className="far fa-trash-alt fa-lg"></i></span>
 						</div>
 					</div>
 				</div>
