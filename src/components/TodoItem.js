3445021286040807d12f7e9251e0c8134d19bbbd
Yassine KLILICH <klilichyassine8@gmail.com>
2019-01-31 22:33:44 +0100

    Enhance the use the checkbox

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,14 +3,13 @@ import React from 'react';
 function TodoItem(props) {
     return (
         <div className="row border border-bottom-0 border-primary rounded">
-            <div className="custom-control custom-checkbox pt pb-2 pr-2">
+            <div className="custom-control custom-checkbox text-truncate col pt">
                 <input type="checkbox" className="custom-control-input" id={"cb" + props.id}/>
-                <label className="custom-control-label" htmlFor={"cb" + props.id}>{/* Don't forget to bring todo title HERE ! */}</label>
+                <label className="custom-control-label pl-3 todo-title col" htmlFor={"cb" + props.id}>{props.todoTitle}</label>
             </div>
-            <span className="todo-text col text-truncate py-2 pl-2">{props.todoTitle}</span>
-            <span className="p-2 pt"><i className="far fa-star fa-lg"></i></span>
-            <span className="p-2 pt"><i className="far fa-edit fa-lg"></i></span>
-            <span className="p-2 pr-3 pt"><i className="far fa-trash-alt fa-lg"></i></span>
+            <span className="p-2 pt"><i className="far fa-star fa-lg icon"></i></span>
+            <span className="p-2 pt"><i className="far fa-edit fa-lg icon"></i></span>
+            <span className="p-2 pr-3 pt"><i className="far fa-trash-alt fa-lg icon"></i></span>
         </div>
     )
 }
