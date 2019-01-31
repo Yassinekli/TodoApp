2221606440422843dc602e63b0e6a543dd11b91d
Yassine KLILICH <klilichyassine8@gmail.com>
2019-01-31 22:57:40 +0100

    Add tasks file

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,11 +1,26 @@
 import React from 'react';
 
 function TodoItem(props) {
+    let textDecorationLine = '';
+
+    if(props.completed)
+        textDecorationLine = 'line-through';
+
     return (
         <div className="row border border-bottom-0 border-primary rounded">
             <div className="custom-control custom-checkbox text-truncate col pt">
-                <input type="checkbox" className="custom-control-input" id={"cb" + props.id}/>
-                <label className="custom-control-label pl-3 todo-title col" htmlFor={"cb" + props.id}>{props.todoTitle}</label>
+                <input 
+                    type="checkbox"
+                    className="custom-control-input" 
+                    id={"cb" + props.id} 
+                    checked={props.completed}
+                    
+                    />
+                <label 
+                    className="custom-control-label pl-3 todo-title col"
+                    htmlFor={"cb" + props.id}
+                    style={{textDecorationLine}}
+                    >{props.todoTitle}</label>
             </div>
             <span className="p-2 pt"><i className="far fa-star fa-lg icon"></i></span>
             <span className="p-2 pt"><i className="far fa-edit fa-lg icon"></i></span>
