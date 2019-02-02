a16d6202b1c5244fda851bf37186ad591570fa29
Yassine KLILICH <klilichyassine8@gmail.com>
2019-02-02 14:21:14 +0100

    Try to add drag & drop for todo item

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,18 +6,12 @@ class TodoItem extends Component {
         this.state = {
             id: props.id,
             todoTitle: props.todoTitle,
-            completed : props.completed
+            completed : props.completed,
+            dragStart : false
         };
     }
     
-    changeHandler = ()=> {
-        this.setState({
-            id: this.state.id,
-            todoTitle: this.state.todoTitle,
-            completed : !this.state.completed
-        })
-    }
-    
+    // Methods
     titleStyle(){
         let titleStyle = {
             textDecorationLine : 'none',
@@ -33,18 +27,82 @@ class TodoItem extends Component {
         return titleStyle;
     }
 
+    dragingPropStyle(){
+        if(!this.state.dragStart)
+            return;
+        return {
+            background : '#dde2ff',
+            zIndex: 1000,
+            top: this.state.Y + "px"
+        }
+    }
+
+    // Event Handlers
+    changeHandler(){
+        this.setState({
+            id: this.state.id,
+            todoTitle: this.state.todoTitle,
+            completed : !this.state.completed,
+            dragStart : false
+        })
+    }
+    
+    dragStartHandler(e){
+        this.setState({
+            id: this.state.id,
+            todoTitle: this.state.todoTitle,
+            completed : this.state.completed,
+            dragStart : true
+        })
+    }
+
+    dragHandler(e){
+        // console.log('X : ', e.clientX, ', Y : ', e.clientY);
+        if( this.state.Y && this.state.Y === e.clientY )
+            return;
+        
+        let targetStyle = window.getComputedStyle(e.target, null);
+        console.log(targetStyle.height);
+        
+        
+        let Y = e.clientY;
+
+        this.setState({
+            id: this.state.id,
+            todoTitle: this.state.todoTitle,
+            completed : this.state.completed,
+            dragStart : true,
+            Y
+        });
+    }
+
+    dragEndHandler(e){
+        this.setState({
+            id: this.state.id,
+            todoTitle: this.state.todoTitle,
+            completed : this.state.completed,
+            dragStart : false
+        });
+    }
+    
+    // Render Method
     render(){
-        
-        
         return (
-            <div className="row border border-bottom-0 border-primary rounded">
+            <div
+                className='row border border-bottom-0 border-primary rounded'
+                style={this.dragingPropStyle()}
+                draggable
+                onDragStart={(e)=>this.dragStartHandler(e)}
+                onDrag={(e)=>this.dragHandler(e)}
+                onDragEnd={(e)=>this.dragEndHandler(e)}
+                >
                 <div className="custom-control custom-checkbox text-truncate col pt">
                     <input 
                         type="checkbox"
                         className="custom-control-input" 
                         id={"cb" + this.state.id}
                         defaultChecked={this.state.completed}
-                        onChange={this.changeHandler}
+                        onChange={()=>this.changeHandler()}
                         />
                     <label 
                         className="custom-control-label pl-3 todo-title col"
