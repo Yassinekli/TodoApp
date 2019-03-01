92aeae7c6eb47451e13b74f15902de48d282c8b8
Yassine KLILICH <klilichyassine8@gmail.com>
2019-03-01 11:54:22 +0100

    Update AddTodoModal.js

--- src/components/AddTodoModal.js
+++ src/components/AddTodoModal.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 
 class AddTodoForm extends Component {
+
+	componentDidUpdate(){
+		if(this.props.showModal)
+			this.textInput.focus();
+	}
 	
 	render() {
 		let modalContainerClassNames = "modal-container";
@@ -10,10 +15,11 @@ class AddTodoForm extends Component {
 		
 		return (
 			<div className={modalContainerClassNames} onClick={(e)=>{
-						if(e.target.classList[0] === "modal-container" || e.target.classList[0] === "far")
-							this.props.onClick();
-					}
-				}>
+				if(e.target.classList[0] === "modal-container" || e.target.classList[0] === "far")
+					this.props.onClick();
+				}
+			}
+			>
 				<div className="modal-form">
 					<div className="modal-head">
 						<span className="modal-close"><i className="far fa-times-circle"></i></span>
@@ -21,7 +27,7 @@ class AddTodoForm extends Component {
 					</div>
 					<hr/>
 					<div className="modal-input clearfix">
-						<input type="text" className="form-control input-title" id="todoTitle"></input>
+						<input type="text" ref={elem=>this.textInput = elem} className="form-control input-title" id="todoTitle"></input>
 						<button className="btn btn-primary btn-add" onClick={()=>this.props.addNewTodo()}>Add</button>
 					</div>
 				</div>
